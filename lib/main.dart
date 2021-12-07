0f10b1e5df354b5d9604e3a8dc55d26fe6c27dbc
Yan Feng <fengyan0121@gmail.com>
2021-12-07 14:38:56 -0500

    Add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/cosmology_calculator_screen.dart';
+import './providers/calculator.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => Calculator(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
